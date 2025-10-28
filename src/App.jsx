cf42b2947d668af64ae552ad6cd65861365109c3
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-10-28 08:50:51 +0100

    doc(App.jsx): añadida descripción más concisa en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1>User Test</h1>
-        <p>Genera errores para probar la captura en Buggle.</p>
+        <p>Genera errores en código (por ejemplo en App.js o main.jsx) para probar la captura en Buggle.</p>
       <div className="card">
         <ErrorButton />
       </div>
